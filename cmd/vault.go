29e0518f842da0e79ebc61d164c914ac1c8f86c7
marcoan <marcoan@br.ibm.com>
2021-01-05 18:02:34 -0300

    vault

--- cmd/vault.go
+++ cmd/vault.go
@@ -19,37 +19,156 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
+	"github.com/marco-ostaska/boringstuff"
+	"github.com/marco-ostaska/uvault"
 	"github.com/spf13/cobra"
 )
 
-// TODO new outside packages for crypt and decryt, realised I will need to perform this more than I intent to
+const (
+	vaultDir  = "bscli"
+	vaultFile = "bscli.vlt"
+	apiKey    = "Bluesight-API-Token"
+)
+
+var vCredential uvault.Credential
 
 // vaultCmd represents the vault command
 var vaultCmd = &cobra.Command{
 	Use:   "vault",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "create or update vault credentials",
+	Long:  `create or update vault credentials.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("vault called")
+		fmt.Printf("%v\n", cmd.Long)
+		if err := cmd.Usage(); err != nil {
+			log.Fatalln(err)
+		}
 	},
+}
+
+var newCmd = &cobra.Command{
+	Use:           "new",
+	Short:         "create new vault.",
+	Long:          `create new vault.`,
+	SilenceErrors: true,
+	Example: `
+  Unix Based OS: (use single quotes)
+      sl1cmd vault new -k 'pass1234' --url 'https://bluesight.com'
+  Windows: (use double quotes)
+      sl1cmd vault new -k "pass1234" --url "https://bluesight.com"
+`,
+	RunE: newVault,
+}
+
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "update an existing vault.",
+	Long:  `update an existing vault.`,
+	Example: `  
+  Unix based OS:  (use single quotes)
+      sl1cmd update -k 'pass1234'
+  Windows: (use double quotes)
+      sl1cmd update -k "pass1234"`,
+	RunE: updateVault,
+}
+
+var deleteCmd = &cobra.Command{
+	Use:           "delete",
+	Short:         "delete an existing vault.",
+	Long:          `delete an existing vault.`,
+	SilenceUsage:  true,
+	SilenceErrors: true,
+	RunE:          deleteVault,
+}
+
+func addCommandUpdateCmd() error {
+	vaultCmd.AddCommand(updateCmd)
+	updateCmd.Flags().StringP("key", "p", "", "API key value")
+
+	return updateCmd.MarkFlagRequired("key")
+
+}
+
+func addCommandNewCmd() error {
+	vaultCmd.AddCommand(newCmd)
+	newCmd.Flags().StringP("key", "p", "", "API key value")
+	newCmd.Flags().String("url", "", "API URI")
+
+	err := newCmd.MarkFlagRequired("key")
+	err1 := newCmd.MarkFlagRequired("url")
+
+	if re := boringstuff.ReturnError(err, err1); re != nil {
+		return re
+	}
+
+	return nil
+}
+
+func newVault(cmd *cobra.Command, args []string) error {
+	keyValue, err := cmd.Flags().GetString("key")
+	uri, err1 := cmd.Flags().GetString("url")
+
+	if re := boringstuff.ReturnError(err, err1); re != nil {
+		return re
+	}
+
+	if err = vCredential.SetInfo(apiKey, keyValue, uri, vaultDir, vaultFile); err != nil {
+		return err
+	}
+
+	fmt.Println("Vault configured ✔")
+	return nil
+}
+
+func updateVault(cmd *cobra.Command, args []string) error {
+	err := vCredential.ReadFile(vaultDir, vaultFile)
+	if err != nil {
+		if strings.Contains(err.Error(), "no such file or directory") {
+			return fmt.Errorf("No credentials found, please try create a new credential vault first ❌")
+		}
+		return err
+	}
+	keyValue, err := cmd.Flags().GetString("key")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Updating credentials", vCredential.URL)
+
+	if err = vCredential.SetInfo(apiKey, keyValue, vCredential.URL, vaultDir, vaultFile); err != nil {
+		return err
+	}
+	fmt.Println("Vault configured ✔")
+	return nil
+
+}
+
+func deleteVault(cmd *cobra.Command, args []string) error {
+	if err := vCredential.UserInfo(vaultDir, vaultFile); err != nil {
+		return fmt.Errorf("%s ❌", err)
+	}
+
+	if err := os.Remove(vCredential.File); err != nil {
+		return fmt.Errorf("%s ❌", err.Error())
+	}
+
+	fmt.Println("Vault deleted ✔")
+	return nil
+
 }
 
 func init() {
 	rootCmd.AddCommand(vaultCmd)
+	vaultCmd.AddCommand(deleteCmd)
 
-	// Here you will define your flags and configuration settings.
+	err := addCommandNewCmd()
+	err1 := addCommandUpdateCmd()
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// vaultCmd.PersistentFlags().String("foo", "", "A help for foo")
+	if re := boringstuff.ReturnError(err, err1); re != nil {
+		log.Fatalln(err)
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// vaultCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ cmd/vault_test.go
@@ -0,0 +1,104 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestNewVault(t *testing.T) {
+	tt := []struct {
+		name     string
+		args     []string
+		expected string
+	}{
+		{"Missing args 1", []string{"vault", "new"}, `required flag(s) "key", "url", not set`},
+		{"Missing args 2", []string{"vault", "new", "-k", "!@#$%^&*key"}, `required flag(s) url" not set`},
+		{"Ok", []string{"vault", "new", "-k", "!@#$%^&*key", "--url", "https://xyz.io"}, ""},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			rootCmd.SetArgs(tc.args)
+			rootCmd.SilenceErrors = true
+			rootCmd.SilenceUsage = true
+			if err := rootCmd.Execute(); err != nil {
+				if err.Error() == tc.expected {
+					t.Skip(err)
+					return
+				}
+				t.Errorf(err.Error())
+			}
+		})
+	}
+
+	t.Run("Check credentials", func(t *testing.T) {
+		keyValue := "!@#$%^&*key"
+		uri := "https://xyz.io"
+		if err := vCredential.ReadFile(vaultDir, vaultFile); err != nil {
+			t.Errorf(err.Error())
+		}
+
+		switch {
+		case vCredential.APIKey != apiKey:
+			t.Errorf("got %v, expected %v", vCredential.APIKey, apiKey)
+		case vCredential.DecryptedKValue != keyValue:
+			t.Errorf("got %v, expected %v", vCredential.DecryptedKValue, keyValue)
+		case vCredential.URL != uri:
+			t.Errorf("got %v, expected %v", vCredential.URL, uri)
+		}
+
+	})
+
+}
+
+func TestUpdateVault(t *testing.T) {
+	tt := []struct {
+		name     string
+		args     []string
+		expected string
+	}{
+		{"Missing args 1", []string{"vault", "update"}, `required flag(s) "key"`},
+		{"OK", []string{"vault", "update", "-k", "mykey"}, `required flag(s) "url" not set`},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			rootCmd.SetArgs(tc.args)
+			rootCmd.SilenceErrors = true
+			rootCmd.SilenceUsage = true
+			if err := rootCmd.Execute(); err != nil {
+				if err.Error() == tc.expected {
+					t.Skip(err)
+					return
+				}
+				t.Errorf(err.Error())
+			}
+		})
+	}
+
+	t.Run("Check credentials", func(t *testing.T) {
+		keyValue := "em7admin"
+		uri := "https://xyz.io"
+		if err := vCredential.ReadFile(vaultDir, vaultFile); err != nil {
+			t.Errorf(err.Error())
+		}
+
+		switch {
+		case vCredential.APIKey != apiKey:
+			t.Errorf("got %v, expected %v", vCredential.APIKey, apiKey)
+		case vCredential.DecryptedKValue != keyValue:
+			t.Errorf("got %v, expected %v", vCredential.DecryptedKValue, keyValue)
+		case vCredential.URL != uri:
+			t.Errorf("got %v, expected %v", vCredential.URL, uri)
+		}
+
+	})
+}
+
+func TestDeleteVault(t *testing.T) {
+
+	rootCmd.SetArgs([]string{"vault", "delete"})
+	if err := rootCmd.Execute(); err != nil {
+		t.Errorf(err.Error())
+	}
+
+}
